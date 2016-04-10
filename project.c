f32619ad2e26f1e6fcf27d82efb98140357d61d4
excelsi0r <nuno_mgl@hotmail.com>
2016-04-10 13:27:05 +0100

    diferent

--- project.c
+++ project.c
@@ -25,31 +25,54 @@ int main(int argc, char* argv[])
 
   char* filepath = "/tmp/files.txt";
   int file;
+  
+  DIR *dirp; 
+  struct dirent *direntp; 
+  struct stat stat_buf; 
+  char *str; 
+  char name[200]; 
 
   if(argc != 2)
   {
     printf("Usage: %s <directory>\n", argv[0]);
     return 1;
   }
-
-    //mkstemp(filepath);
-    file = open(filepath, O_RDWR | O_APPEND | O_CREAT, 0600);
-
   
-
-
-  if(file == -1) 
-  {
-      perror("/tmp");
-      //perror("Redirecting to console...\n");
-      //file = STDOUT_FILENO;
-  }
-/*
-  char *header = "Serial number | File size | Modification Date | File name\n";
-  write(file, header, strlen(header));
-  //print_directory(argv[1], file);
-  return 0;
-*/
+  file = open(filepath, O_RDWR | O_APPEND | O_CREAT, 0600);
+   
+  if((dirp = opendir( argv[1])) == NULL) 
+   { 
+      perror(argv[1]); 
+      exit(2); 
+     } 
+    while
+     ((direntp = readdir( dirp)) != NULL) 
+     { 
+      sprintf(name,"%s/%s",argv[1],direntp->d_name); 
+    // <----- NOTAR 
+                              // alternativa a chdir(); ex: anterior 
+    if
+     (
+    lstat
+    (name, &stat_buf)==-1)   
+    // testar com 
+    stat()
+      { 
+       perror("lstat ERROR"); 
+       exit(3); 
+      } 
+    //      printf("%10d - ",(int) stat_buf.st_ino); 
+    if
+     (S_ISREG(stat_buf.st_mode)) str = "regular"; 
+    else
+    if
+     (S_ISDIR(stat_buf.st_mode)) str = "directory"; 
+    else
+     str = "other"; 
+      printf("%-25s - %s\n", direntp->d_name, str); 
+     } 
+     closedir(dirp); 
+     exit(0);   
   return 0;
 }
 
